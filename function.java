d5d81f5093c7994c336b8be3e55a7b7de623d865
Rohit Kumar Yadav <your_email@example.com>
2025-07-30 23:01:14 +0530

    first commit

--- function.java
+++ function.java
@@ -3,8 +3,14 @@ public class function {
         return a+b;
 
     }
+    public static int multiply(int a ,int b){
+        return a*b;
+
+
+    }
     public static void main(String[] args) {
         System.out.println(sum(3, 8));
+        System.out.println(multiply(4, 9));
         
     }
 
