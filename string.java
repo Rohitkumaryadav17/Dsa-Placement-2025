0960f0ca81f59d28d2393977fe3ab40a36a08549
Rohit Kumar Yadav <rky5825@gmail.com>
2025-08-05 22:42:34 +0530

    string

--- string.java
+++ string.java
@@ -1,12 +1,14 @@
 public class string {
     public static void main(String[] args) {
         String name="Rohit kumar Yadav";
-        String name1="kapil kumar Gupta";
-        System.out.println(name.equals(name1));
+        // String name1="Rohit kumar Yadav";
+        // System.out.println(name.equals(name1));
     
 
 
         // System.out.println(name.length());
+         System.out.println(name);
+    
 
 
    
