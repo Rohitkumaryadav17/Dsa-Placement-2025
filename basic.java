28fe18823ae5a5de80b41a549b5b6435c19e3bd7
Rohit Kumar Yadav <rky5825@gmail.com>
2025-08-09 23:43:30 +0530

    string

--- basic.java
+++ basic.java
@@ -1,12 +1,24 @@
 public class basic{
-    public static void main(String[] args) {
-        int a=8;
-        int b=9;
-        int sum=a+b;
-        System.out.println(sum);
-        System.out.println(a);
+    public static void reverse(int arr[]){
+        int start=0;
+        int end=arr.length-1;
+        while(start<end){
+            // swapp
+            int temp=arr[start];
+             arr[start]=arr[end];
+             arr[end]=temp;
+             start++;
+             end--;
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    
 
-        // System.out.println("Dsa Learner");
+    }
+    public static void main(String[] args) {
+        int arr[]={3,4,5,6,8,9};
+     reverse(arr); 
 
     }
 }
