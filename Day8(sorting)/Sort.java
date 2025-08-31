30263982929ad2ca5b34928a9a487a671c708825
Rohit Kumar Yadav <rky5825@gmail.com>
2025-08-31 09:15:02 +0530

    mernstack

---
+++ Day8(sorting)/Sort.java
@@ -0,0 +1,32 @@
+public class Sort{
+    public static void sort(int arr[]) {
+        for(int i=0; i<arr.length-1; i++) {
+            for(int j=0; j<arr.length-1-i; j++) {
+                if(arr[j] > arr[j+1]) {
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArr(int arr[]) {
+        for(int i=0; i<arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {2, 4, 6, 1, 5};
+        System.out.println("Unsorted array:");
+        printArr(arr);
+
+        sort(arr);
+
+        System.out.println("Sorted array:");
+        printArr(arr);
+    }
+}
