608d2a67fac42b1fe2711afbfc1045b8155cd14b
Rohit Kumar Yadav <rky5825@gmail.com>
2025-09-07 23:23:27 +0530

    dsa

---
+++ Day 1 (Basic to loop)/factorial.java
@@ -0,0 +1,15 @@
+public class factorial {
+    public static int fact(int n){
+      if(n==1){
+            return 1;
+        }
+        int a = n*fact(n-1);
+        return a;
+    }
+    
+    public static void main(String[]args){
+        System.out.println(fact(8));
+
+    }
+    
+}
