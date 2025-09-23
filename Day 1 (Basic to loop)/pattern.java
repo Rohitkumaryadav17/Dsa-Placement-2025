fb0b339f2a1fe72c605a6457d33c576319fdcb7a
Rohit Kumar Yadav <rky5825@gmail.com>
2025-09-23 23:17:38 +0530

    dsa

--- Day 1 (Basic to loop)/pattern.java
+++ Day 1 (Basic to loop)/pattern.java
@@ -2,7 +2,7 @@ public class pattern {
     public static void main(String[]args){
         for(int i=1;i<6;i--){
             for(int j=1;i<6;j++){
-                System.out.println("*");
+                System.out.println("$");
 
             
         }
