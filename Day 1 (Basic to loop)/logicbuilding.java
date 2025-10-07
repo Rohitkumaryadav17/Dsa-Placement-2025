db4e48dc8d8878390f8ae4d81cca97c36d76e2a1
Rohitkumaryadav17 <rky5825@gmail.com>
2025-10-08 00:58:41 +0530

    dsa

--- Day 1 (Basic to loop)/logicbuilding.java
+++ Day 1 (Basic to loop)/logicbuilding.java
@@ -1,29 +1,43 @@
-import java.util.*;
-public class logicbuilding {
-    public static boolean isEven(int n){
-        int rem=n%2;
-        if(rem==0){
-          return true;
-          }
-          else{
-            return false;
-          }
+// import java.util.*;
+// public class logicbuilding {
+//     public static boolean isEven(int n){
+//         int rem=n%2;
+//         if(rem==0){
+//           return true;
+//           }
+//           else{
+//             return false;
+//           }
 
-    }
+//     }
 
-    public static void main(String[]args){
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Enter your number : ");
-        int n=sc.nextInt();
-         if(isEven(n)==true){
-            System.out.print("even");
-         }
-         else{
-             System.out.print("odd");
-         }
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         System.out.print("Enter your number : ");
+//         int n=sc.nextInt();
+//          if(isEven(n)==true){
+//             System.out.print("even");
+//          }
+//          else{
+//              System.out.print("odd");
+//          }
   
        
 
-    }
+//     }
     
+// }
+import java.util.*;
+
+public class logicbuilding {
+    public static boolean isEven(int n) {
+        return n % 2 == 0; // directly return the condition
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your number: ");
+        int n = sc.nextInt();
+        System.out.println(isEven(n) ? "even" : "odd");
+    }
 }
