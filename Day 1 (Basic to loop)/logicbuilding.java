047979e41214d44da00b39c544103532bec2afab
Rohitkumaryadav17 <rky5825@gmail.com>
2025-10-08 13:17:31 +0530

    dsa

--- Day 1 (Basic to loop)/logicbuilding.java
+++ Day 1 (Basic to loop)/logicbuilding.java
@@ -34,16 +34,31 @@ public class logicbuilding {
       return sum;
       }
 
+      ////// swapp ot two number
+      ///  
+      public static void swappoftwonum(int a,int b){
+           int temp=a;
+           a=b;
+          b=temp;
+                System.out.print("After swapping: a = " + a + ", b = " + b);
+          }
+   
+      
 
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your number: ");
-        int n = sc.nextInt();
+        System.out.print("Enter your number a: ");
+        int a = sc.nextInt();
+           System.out.print("Enter your number b: ");
+           int b=sc.nextInt();
+          swappoftwonum(a,b);
+        
         // System.out.println(isEven(n) ? "even" : "odd");
         // printtable(n);
-         System.out.println(findsum(n));
-         System.out.println(sumofsquarenaturalno(n));
+        //  System.out.println(findsum(n));
+        //  System.out.println(sumofsquarenaturalno(n));
+
    
     }
 }
