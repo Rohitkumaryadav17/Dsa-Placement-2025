5ceefe173f8391f30a5136bb16ed066516c6dacf
Rohitkumaryadav17 <rky5825@gmail.com>
2025-10-08 12:47:55 +0530

    dsa

--- Day 1 (Basic to loop)/logicbuilding.java
+++ Day 1 (Basic to loop)/logicbuilding.java
@@ -1,43 +1,23 @@
-// import java.util.*;
-// public class logicbuilding {
-//     public static boolean isEven(int n){
-//         int rem=n%2;
-//         if(rem==0){
-//           return true;
-//           }
-//           else{
-//             return false;
-//           }
-
-//     }
-
-//     public static void main(String[]args){
-//         Scanner sc=new Scanner(System.in);
-//         System.out.print("Enter your number : ");
-//         int n=sc.nextInt();
-//          if(isEven(n)==true){
-//             System.out.print("even");
-//          }
-//          else{
-//              System.out.print("odd");
-//          }
-  
-       
-
-//     }
-    
-// }
 import java.util.*;
 
 public class logicbuilding {
+
+     ////print even or odd number
+    
     public static boolean isEven(int n) {
         return n % 2 == 0; // directly return the condition
     }
+
+        /// print table of any number
+      
       public static void printtable(int n){
         for(int i=1;i<10;++i){
             System.out.println(n+"*"+i+"="+n*i);
         }
       }
+
+      /// find sum of n natural number
+    
       public static int findsum(int n){
         int sum=0;
         for(int i=0;i<=n;i++){
@@ -46,6 +26,15 @@ public class logicbuilding {
         return sum;
       
       }
+      public static int sumofsquarenaturalno(int n){
+         int sum=0;
+      for(int i=0;i<n+1;i++){
+        sum=i*i+sum;
+      }
+      return sum;
+      }
+
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -54,6 +43,7 @@ public class logicbuilding {
         // System.out.println(isEven(n) ? "even" : "odd");
         // printtable(n);
          System.out.println(findsum(n));
+         System.out.println(sumofsquarenaturalno(n));
    
     }
 }
