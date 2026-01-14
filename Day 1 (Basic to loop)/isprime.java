1243a4b6bfcb7ee036425ba8e4ad0b5b24d377ee
Rohit kumar yadav <rky5825@gmail.com>
2026-01-14 07:30:38 +0530

    dsa

---
+++ Day 1 (Basic to loop)/isprime.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class isprime {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.err.println("enter number : ");
+
+        int n=sc.nextInt();
+        if(n==2){
+            System.out.println("prime ");
+        }
+       
+            else{
+                
+                 boolean isprime=true;
+        for(int i=2;i<=n-1;i++){
+            if(n%i==0){
+                isprime=false;
+
+            }
+        }
+        if(isprime==true){
+            System.out.println("n is prime ");
+        }
+        else{
+            System.out.println("not prime");
+        }
+            }
+
+
+
+    }
+    
+}
--- Day 1 (Basic to loop)/reverse.java
+++ Day 1 (Basic to loop)/reverse.java
@@ -1,6 +1,6 @@
 public class reverse{
     public static void main(String[]args){
-    int n=10894;
+    int n=10899;
     while(n>0){
          int lastdigit=n%10;
          System.out.print(lastdigit+ " ");
