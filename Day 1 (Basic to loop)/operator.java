e1b74e8288ee1384ceba24470d22e1f4ecdbc505
Rohit kumar yadav <rky5825@gmail.com>
2026-02-11 15:42:06 +0530

    operator

---
+++ Day 1 (Basic to loop)/operator.java
@@ -0,0 +1,48 @@
+class operator{
+    public static void main(String[] args) {
+        // int a=4, b=5;
+        // double arithematic=(a+b-a*b)/a%2;
+      // System.out.println("arithmatic  is : "+arithematic);
+      // binary operator 
+       // a=a+1 or a++ a=a-1 or a--
+    //    int a=5;
+    //    int b=a--;
+
+    
+    //     System.out.println(a);
+    //     System.out.println(b);
+     // relation operator  ==, !=, >,<,>=,<=
+
+    //  int a=6;
+    //  int b=8;
+    //   System.out.println(a!=b);
+          /// logical operator  &&, ||, !
+    //    System.out.println((4>2) && (8>9)); 
+    //    System.out.println((1<8)||(9<5));  // all case give true exepet F , F statment 
+    //   System.out.println((6>7)!=(4<9));
+    //    System.out.println((1>0)&&(3>2));
+     /// assinment operator   = ,+=,-=,*=,/=
+       int a=8,b=9; 
+    //    System.out.println(a=b);
+    // a=a+10 or a+=10  a=a-2  a-=2
+    //   System.out.println(a-=6);
+        // a+=2;  // a=a+a;
+        // a-=5;
+        // b%=8;
+
+        // System.out.println(b);
+        
+        // ternary operator 
+        //  varible=condition ? statement 1: statement 2;
+       int number=9;
+       // ternary operator
+        String type =((number%2)== 0)?"even": "odd";
+        System.out.println(type);
+           
+ 
+   
+
+
+        
+    }
+}
